e178b5329a6e93b0f7df9dc60d2d6e98210a90a3
DMGreyMouser <159208728+DmGreyMouser@users.noreply.github.com>
2025-09-22 18:57:40 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
+import { FormRenderer } from './modules/FormRenderer.js';
+import { PromptGenerator } from './modules/PromptGenerator.js';
 
+class RealtorToolsApp {
+    constructor() {
+        this.formRenderer = new FormRenderer();
+        this.promptGenerator = new PromptGenerator();
+        this.init();
+    }
+
+    async init() {
+        await this.loadComponents();
+        this.setupEventListeners();
+    }
+
+    async loadComponents() {
+        const header = await fetch('components/header.html').then(r => r.text());
+        document.getElementById('app-header').innerHTML = header;
+        
+        // Charger les autres composants...
+    }
+}
+
+new RealtorToolsApp();
